971a77d7b173ac2f20f7c09bb4d77691a5b8723b
MostafaHamed-W <mostafa.hamed.w@gmail.com>
2024-02-06 17:34:15 +0200

    Add optionURLReflectsImperativeAPIs

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:cart_scope/src/app.dart';
 import 'package:cart_scope/src/localization/string_hardcoded.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
+import 'package:go_router/go_router.dart';
 
 void main() async {
   // * For more info on error handling, see:
@@ -11,6 +12,9 @@ void main() async {
 
   // turn off the # in the URLs on the web
   usePathUrlStrategy();
+
+  // to make pushNamed or push effects the url we do this afer V8.0.0
+  GoRouter.optionURLReflectsImperativeAPIs = true;
 
   await runZonedGuarded(
     () async {
