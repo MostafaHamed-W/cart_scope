982100950c08ec189267c1e64373b9d8af878ffa
MostafaHamed-W <mostafa.hamed.w@gmail.com>
2024-02-10 16:00:03 +0200

    Update flutter error handler & install riverpod package

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,44 @@
-import 'dart:async';
+import 'dart:ui';
 import 'package:cart_scope/src/app.dart';
 import 'package:cart_scope/src/localization/string_hardcoded.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'package:flutter_web_plugins/url_strategy.dart';
+import 'package:go_router/go_router.dart';
 
 void main() async {
-  // * For more info on error handling, see:
+  WidgetsFlutterBinding.ensureInitialized();
+  // turn off the # in the URLs on the web
+  usePathUrlStrategy();
+  // * Register error handlers. For more info, see:
   // * https://docs.flutter.dev/testing/errors
-  await runZonedGuarded(() async {
-    WidgetsFlutterBinding.ensureInitialized();
-    // turn off the # in the URLs on the web
-    usePathUrlStrategy();
+  registerErrorHandlers();
+  // * Entry point of the app
+  runApp(const ProviderScope(child: CartScope()));
+  // *to make pushNamed or push effects the url we do this afer V8.0.0
+  GoRouter.optionURLReflectsImperativeAPIs = true;
+}
 
-    // to make pushNamed or push effects the url we do this afer V8.0.0
-    GoRouter.optionURLReflectsImperativeAPIs = true;
-    // * Entry point of the app
-    runApp(const CartScope());
-
-    // * This code will present some error UI if any uncaught exception happens
-    FlutterError.onError = (FlutterErrorDetails details) {
-      FlutterError.presentError(details);
-    };
-    ErrorWidget.builder = (FlutterErrorDetails details) {
-      return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.red,
-          title: Text('An error occurred'.hardcoded),
-        ),
-        body: Center(child: Text(details.toString())),
-      );
-    };
-  }, (Object error, StackTrace stack) {
-    // * Log any errors to console
+void registerErrorHandlers() {
+  // * Show some error UI if any uncaught exception happens
+  FlutterError.onError = (FlutterErrorDetails details) {
+    FlutterError.presentError(details);
+    debugPrint(details.toString());
+  };
+  // * Handle errors from the underlying platform/OS
+  PlatformDispatcher.instance.onError = (Object error, StackTrace stack) {
     debugPrint(error.toString());
-  });
+    return true;
+  };
+  // * Show some error UI when any widget in the app fails to build
+  ErrorWidget.builder = (FlutterErrorDetails details) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text('An error occurred'.hardcoded),
+      ),
+      body: Center(child: Text(details.toString())),
+    );
+  };
 }
