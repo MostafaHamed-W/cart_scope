b76d886a68c463450c8b304fac0192ff574ba0df
MostafaHamed-W <mostafa.hamed.w@gmail.com>
2024-02-14 20:53:01 +0200

    Add abstraction to AuthRepository

--- lib/src/features/authentication/data/fake_auth_repository.dart
+++ lib/src/features/authentication/data/fake_auth_repository.dart
@@ -1,25 +1,72 @@
 import 'package:cart_scope/src/features/authentication/domain/app_user.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class FakeAuthRepository {
+abstract class AuthRepository {
+  Stream<AppUser?> authStateChanges();
+  AppUser? get currentUser;
+  Future<void> signInWithEmailAndPassword(String email, String password);
+  Future<void> createEmailWithEmailAndPassword(String emial, String password);
+  Future<void> signOut();
+}
+
+class FakeAuthRepository implements AuthRepository {
+  @override
   Stream<AppUser?> authStateChanges() => Stream.value(null); //TODO: implement
 
+  @override
   AppUser? get currentUser => null; //TODO: implement
 
+  @override
   Future<void> signInWithEmailAndPassword(String email, String password) async {
     //TODO: implement
   }
 
+  @override
   Future<void> createEmailWithEmailAndPassword(String emial, String password) async {
     //TODO: implement
   }
 
+  @override
   Future<void> signOut() async {
     //TODO: implement
   }
 }
 
-final authRepositoryProvider = Provider<FakeAuthRepository>((ref) {
+class FirebaseAuthRepository implements AuthRepository {
+  @override
+  Stream<AppUser?> authStateChanges() {
+    // TODO: implement authStateChanges
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> createEmailWithEmailAndPassword(String emial, String password) {
+    // TODO: implement createEmailWithEmailAndPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  // TODO: implement currentUser
+  AppUser? get currentUser => throw UnimplementedError();
+
+  @override
+  Future<void> signInWithEmailAndPassword(String email, String password) {
+    // TODO: implement signInWithEmailAndPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> signOut() {
+    // TODO: implement signOut
+    throw UnimplementedError();
+  }
+}
+
+//we can run with command: —dart-define=useFakeRepos=true to run with string from environment
+
+final authRepositoryProvider = Provider<AuthRepository>((ref) {
+  // final isFake = String.fromEnvironment("useFakeRepos") == true;
+  // return isFake ? FakeAuthRepository() : FirebaseAuthRepository();
   return FakeAuthRepository();
 });
 
