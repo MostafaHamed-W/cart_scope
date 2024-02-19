46c3006bc2d08bb4160181471fe05e7017996ae5
Mostafa Hamed <mostafa.hamed.w@gmail.com>
2024-02-19 13:44:56 +0200

    Migrate from StateNotifier to Notifier

--- lib/src/features/authentication/presentation/account/account_screen_controller.dart
+++ lib/src/features/authentication/presentation/account/account_screen_controller.dart
@@ -1,10 +1,15 @@
 import 'package:cart_scope/src/features/authentication/data/fake_auth_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class AccountScreenController extends StateNotifier<AsyncValue<void>> {
-  AccountScreenController(this.fakeAuthRepository) : super(const AsyncValue<void>.data(null));
+class AccountScreenController extends Notifier<AsyncValue<void>> {
+  late FakeAuthRepository fakeAuthRepository;
 
-  final FakeAuthRepository fakeAuthRepository;
+  @override
+  AsyncValue<void> build() {
+    fakeAuthRepository = ref.watch(authRepositoryProvider);
+    return const AsyncValue.data(null);
+  }
+
   Future<void> signOut() async {
     state = const AsyncValue.loading();
     await Future.delayed(const Duration(seconds: 2));
@@ -18,7 +23,6 @@ class AccountScreenController extends StateNotifier<AsyncValue<void>> {
 }
 
 final accountScreenControllerProvider =
-    StateNotifierProvider<AccountScreenController, AsyncValue<void>>((ref) {
-  final fakeAuthRepository = ref.watch(authRepositoryProvider);
-  return AccountScreenController(fakeAuthRepository);
+    NotifierProvider<AccountScreenController, AsyncValue<void>>(() {
+  return AccountScreenController();
 });
