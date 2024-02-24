4c7dfcbae66d915f668a22cca407af6216f81d4a
Mostafa Hamed <mostafa.hamed.w@gmail.com>
2024-02-24 23:59:34 +0200

    Reset GoRouter Preferences

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,13 @@ import 'package:cart_scope/src/app.dart';
 import 'package:cart_scope/src/localization/string_hardcoded.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_web_plugins/url_strategy.dart';
+// import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:go_router/go_router.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // turn off the # in the URLs on the web
-  usePathUrlStrategy();
+  // usePathUrlStrategy();
   // * Register error handlers. For more info, see:
   // * https://docs.flutter.dev/testing/errors
   registerErrorHandlers();
