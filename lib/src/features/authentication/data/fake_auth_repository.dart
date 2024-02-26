f47d7c21d3033962ffc922fb7ac95bcf9d7c616a
Mostafa Hamed <mostafa.hamed.w@gmail.com>
2024-02-26 16:25:43 +0200

    Complete testing coverage - FakeAuthRepository

--- lib/src/features/authentication/data/fake_auth_repository.dart
+++ lib/src/features/authentication/data/fake_auth_repository.dart
@@ -1,8 +1,10 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'package:cart_scope/src/features/authentication/domain/app_user.dart';
 import 'package:cart_scope/src/localization/string_hardcoded.dart';
 import 'package:cart_scope/src/utils/delay.dart';
 import 'package:cart_scope/src/utils/in_memory_store.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 abstract class AuthRepository {
   Stream<AppUser?> authStateChanges();
@@ -14,6 +16,10 @@ abstract class AuthRepository {
 
 class FakeAuthRepository implements AuthRepository {
   final _authState = InMemoryStore<AppUser?>(null);
+  final bool isDelayed;
+  FakeAuthRepository({
+    this.isDelayed = true,
+  });
 
   @override
   Stream<AppUser?> authStateChanges() => _authState.stream;
@@ -23,7 +29,7 @@ class FakeAuthRepository implements AuthRepository {
 
   @override
   Future<void> signInWithEmailAndPassword(String email, String password) async {
-    await Future.delayed(const Duration(seconds: 2));
+    await delay(true);
     if (currentUser == null) {
       _createNewUser(email);
     }
@@ -31,7 +37,7 @@ class FakeAuthRepository implements AuthRepository {
 
   @override
   Future<void> createEmailWithEmailAndPassword(String email, String password) async {
-    await delay(true);
+    await delay(isDelayed);
     if (currentUser == null) {
       _createNewUser(email);
     }
@@ -39,7 +45,7 @@ class FakeAuthRepository implements AuthRepository {
 
   @override
   Future<void> signOut() async {
-    await delay(true);
+    await delay(isDelayed);
     // throw Exception('Error signning in!');
     _authState.value = null;
   }
--- test/src/features/authentication/data/fake_auth_repository_test.dart
+++ test/src/features/authentication/data/fake_auth_repository_test.dart
@@ -3,26 +3,63 @@ import 'package:cart_scope/src/features/authentication/domain/app_user.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  FakeAuthRepository makeAuthRepository() => FakeAuthRepository();
+  FakeAuthRepository makeAuthRepository() => FakeAuthRepository(isDelayed: false);
 
   String testEmail = 'wafy@gmail.com';
   String testPassword = '12345678';
   String testUid = testEmail.split('').reversed.join();
   AppUser testUser = AppUser(uid: testUid, email: testEmail);
 
-  group('Test FakeAuthRepository', () {
-    final fakeAuthRepository = makeAuthRepository();
+  group(
+    'Test FakeAuthRepository',
+    () {
+      test('test currentUser with null return', () {
+        final fakeAuthRepository = makeAuthRepository();
+        expect(fakeAuthRepository.currentUser, null);
+        expect(fakeAuthRepository.authStateChanges(), emits(null));
+      });
 
-    test('test currentUser with null return', () {
-      expect(fakeAuthRepository.currentUser, null);
-      expect(fakeAuthRepository.authStateChanges(), emits(null));
-    });
-  });
+      test('Test FakeAuthRepository after signin', () async {
+        final fakeAuthRepository = makeAuthRepository();
+        await fakeAuthRepository.signInWithEmailAndPassword(testEmail, testPassword);
+        expect(fakeAuthRepository.currentUser, testUser);
+        expect(fakeAuthRepository.authStateChanges(), emits(testUser));
+      });
 
-  test('Test FakeAuthRepository with AppUser value', () async {
-    final fakeAuthRepository = makeAuthRepository();
-    await fakeAuthRepository.signInWithEmailAndPassword(testEmail, testPassword);
-    expect(fakeAuthRepository.currentUser, testUser);
-    expect(fakeAuthRepository.authStateChanges(), emits(testUser));
-  });
+      test(
+        'Test FakeAuthRepository after signup',
+        () async {
+          final fakeAuthRepository = makeAuthRepository();
+          await fakeAuthRepository.createEmailWithEmailAndPassword(testEmail, testPassword);
+          expect(fakeAuthRepository.currentUser, testUser);
+          expect(fakeAuthRepository.authStateChanges(), emits(testUser));
+        },
+      );
+
+      test(
+        'Current user is null after sign out',
+        () async {
+          final fakeAuthRepository = makeAuthRepository();
+          await fakeAuthRepository.signInWithEmailAndPassword(testEmail, testPassword);
+          expect(fakeAuthRepository.currentUser, testUser);
+          expect(fakeAuthRepository.authStateChanges(), emits(testUser));
+          await fakeAuthRepository.signOut();
+          expect(fakeAuthRepository.currentUser, null);
+          expect(fakeAuthRepository.authStateChanges(), emits(null));
+        },
+      );
+
+      test(
+        'Sign in method after dispose throw an exception',
+        () async {
+          final fakeAuthRepository = makeAuthRepository();
+          fakeAuthRepository.dispose();
+          expect(
+            () => fakeAuthRepository.signInWithEmailAndPassword(testEmail, testPassword),
+            throwsStateError,
+          );
+        },
+      );
+    },
+  );
 }
