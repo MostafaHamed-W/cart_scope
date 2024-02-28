4201ccce0a8e1540684ece2922275dc3d9d54307
Mostafa Hamed <mostafa.hamed.w@gmail.com>
2024-02-28 16:20:25 +0200

    Completing EmailAndPasswordScreenController Unit Testing

---
+++ test/mocks.dart
@@ -0,0 +1,4 @@
+import 'package:cart_scope/src/features/authentication/data/fake_auth_repository.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockAuthRepository extends Mock implements FakeAuthRepository {}
--- test/src/features/authentication/presentation/account/account_screen_controller_test.dart
+++ test/src/features/authentication/presentation/account/account_screen_controller_test.dart
@@ -4,7 +4,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
-class MockAuthRepository extends Mock implements FakeAuthRepository {}
+import '../../../../../mocks.dart';
 
 void main() {
   late FakeAuthRepository authRepository;
