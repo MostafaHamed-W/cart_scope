42ba9b425dd5cca3c6717aa044f9e0d7412e517d
Mostafa Hamed <mostafa.hamed.w@gmail.com>
2024-05-09 20:25:43 +0300

    Adding json serialization & toString to Cart Class

--- lib/src/features/cart/domain/cart.dart
+++ lib/src/features/cart/domain/cart.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
 import 'package:cart_scope/src/features/cart/domain/item.dart';
 import 'package:cart_scope/src/features/products/domain/product.dart';
 
@@ -9,6 +12,27 @@ class Cart {
   /// - key: product ID
   /// - value: quantity
   final Map<ProductID, int> items;
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'items': items,
+    };
+  }
+
+  factory Cart.fromMap(Map<String, dynamic> map) {
+    return Cart(
+      Map<ProductID, int>.from(
+        (map['items'] as Map<ProductID, int>),
+      ),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Cart.fromJson(String source) => Cart.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() => 'Cart(items: $items)';
 }
 
 extension CartItems on Cart {
