ea1c5b8967e24562ac74d803f07501c7563f4da2
Mostafa Hamed <mostafa.hamed.w@gmail.com>
2024-05-11 17:31:31 +0300

    Add setItem, addItem and deleteItem mothods to CartService

--- lib/src/features/cart/application/cart_service.dart
+++ lib/src/features/cart/application/cart_service.dart
@@ -2,6 +2,9 @@ import 'package:cart_scope/src/features/authentication/data/fake_auth_repository
 import 'package:cart_scope/src/features/cart/data/local/local_cart_repository.dart';
 import 'package:cart_scope/src/features/cart/data/remote/remote_cart_repository.dart';
 import 'package:cart_scope/src/features/cart/domain/cart.dart';
+import 'package:cart_scope/src/features/cart/domain/item.dart';
+import 'package:cart_scope/src/features/cart/domain/mutable_cart.dart';
+import 'package:cart_scope/src/features/products/domain/product.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class CartService {
@@ -31,6 +34,27 @@ class CartService {
     } else {
       await localCartRepository.setCart(cart);
     }
+  }
+
+  // Set an item in local or remote repository depending on the user auth state
+  Future<void> setItem(Item item) async {
+    final cart = await _fetchCart();
+    final updated = cart.setItem(item);
+    await setCart(updated);
+  }
+
+  // Add an item in local or remote repository depending on the user auth state
+  Future<void> addItem(Item item) async {
+    final cart = await _fetchCart();
+    final updated = cart.addItem(item);
+    await setCart(updated);
+  }
+
+  // Remove an item from local or remote repository depending on the user auth state
+  Future<void> removeItem(ProductID productID) async {
+    final cart = await _fetchCart();
+    final updated = cart.removeItemById(productID);
+    await setCart(updated);
   }
 }
 
