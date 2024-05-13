30e589076e76ea91446cfc40b7a7b2d03f7dfd16
Mostafa Hamed <mostafa.hamed.w@gmail.com>
2024-05-13 18:54:18 +0300

    Add equallity to item class

--- lib/src/features/cart/domain/item.dart
+++ lib/src/features/cart/domain/item.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:cart_scope/src/features/products/domain/product.dart';
 
 /// A product along with a quantity that can be added to an order/cart
@@ -8,4 +9,17 @@ class Item {
   });
   final ProductID productId;
   final int quantity;
+
+  @override
+  bool operator ==(covariant Item other) {
+    if (identical(this, other)) return true;
+
+    return other.productId == productId && other.quantity == quantity;
+  }
+
+  @override
+  int get hashCode => productId.hashCode ^ quantity.hashCode;
+
+  @override
+  String toString() => 'Item(productId: $productId, quantity: $quantity)';
 }
