bc058d8f115dde9bf31054edcb830fbf3b0bfeb8
Mostafa Hamed <mostafa.hamed.w@gmail.com>
2024-05-24 19:05:48 +0300

    Edit checkout screen preparing checkout feature

--- lib/src/features/checkout/presentation/checkout_screen/checkout_screen.dart
+++ lib/src/features/checkout/presentation/checkout_screen/checkout_screen.dart
@@ -5,17 +5,12 @@ import 'package:cart_scope/src/localization/string_hardcoded.dart';
 import 'package:flutter/material.dart';
 
 /// The two sub-routes that are presented as part of the checkout flow.
-/// TODO: add the address page as well (see [AddressScreen]).
 enum CheckoutSubRoute { register, payment }
 
 /// This is the root widget of the checkout flow, which is composed of 2 pages:
 /// 1. Register page
 /// 2. Payment page
-/// The correct page is displayed (and updated) based on whether the user is
-/// signed in.
-/// The logic for the entire flow is implemented in the
-/// [CheckoutScreenController], while UI updates are handled by a
-/// [PageController].
+/// TODO: Show the correct page based on whether the user is signed in.
 class CheckoutScreen extends StatefulWidget {
   const CheckoutScreen({super.key});
 
@@ -27,6 +22,7 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
   final _controller = PageController();
 
   var _subRoute = CheckoutSubRoute.register;
+  // TODO: Load the correct initial page when this screen is presented
 
   @override
   void dispose() {
@@ -47,9 +43,7 @@ class _CheckoutScreenState extends State<CheckoutScreen> {
   @override
   Widget build(BuildContext context) {
     // map subRoute to address
-    final title = _subRoute == CheckoutSubRoute.register
-        ? 'Register'.hardcoded
-        : 'Payment'.hardcoded;
+    final title = _subRoute == CheckoutSubRoute.register ? 'Register'.hardcoded : 'Payment'.hardcoded;
     // * Return a Scaffold with a PageView containing the 2 pages.
     // * This allows for a nice scroll animation when switching between pages.
     // * Note: only the currently active page will be visible.
