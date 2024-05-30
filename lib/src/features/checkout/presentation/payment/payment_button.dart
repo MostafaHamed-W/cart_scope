04351dda02e6d55ba9bc2bc534ceb78d43b9a2c8
Mostafa Hamed <mostafa.hamed.w@gmail.com>
2024-05-30 20:50:14 +0300

    Using paymentButtonController in PaymentButton & Error handling

--- lib/src/features/checkout/presentation/payment/payment_button.dart
+++ lib/src/features/checkout/presentation/payment/payment_button.dart
@@ -1,25 +1,24 @@
-import 'package:cart_scope/src/common_widgets/alert_dialogs.dart';
+import 'package:cart_scope/src/features/checkout/presentation/payment/payment_button_controller.dart';
 import 'package:cart_scope/src/localization/string_hardcoded.dart';
+import 'package:cart_scope/src/utils/async_value_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:cart_scope/src/common_widgets/primary_button.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 /// Button used to initiate the payment flow.
-class PaymentButton extends StatelessWidget {
+class PaymentButton extends ConsumerWidget {
   const PaymentButton({super.key});
 
-  Future<void> _pay(BuildContext context) async {
-    // TODO: Implement
-    showNotImplementedAlertDialog(context: context);
-  }
-
   @override
-  Widget build(BuildContext context) {
-    // TODO: error handling
-    // TODO: loading state
+  Widget build(BuildContext context, WidgetRef ref) {
+    // Error handling
+    ref.listen<AsyncValue>(paymentButtonControllerProvider, (_, state) => state.showAlertDialogOnError(context));
+    // Loading state
+    final state = ref.read(paymentButtonControllerProvider);
     return PrimaryButton(
       text: 'Pay'.hardcoded,
-      isLoading: false,
-      onPressed: () => _pay(context),
+      isLoading: state.isLoading,
+      onPressed: () => state.isLoading ? null : ref.read(paymentButtonControllerProvider.notifier).pay(),
     );
   }
 }
