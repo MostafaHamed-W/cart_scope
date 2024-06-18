dde4a845b96104a2dceacc11abad7c63091d7358
Mostafa Hamed <mostafa.hamed.w@gmail.com>
2024-06-18 20:40:41 +0300

    Adding AppExceptionDetails class to clarify exception with user friendly messages

--- lib/src/exceptions/app_exception.dart
+++ lib/src/exceptions/app_exception.dart
@@ -1,3 +1,4 @@
+import 'package:cart_scope/src/localization/string_hardcoded.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'app_exception.freezed.dart';
@@ -15,4 +16,52 @@ class AppException with _$AppException {
   const factory AppException.paymentFailureEmptyCart() = PaymentFailureEmptyCart;
   // Orders
   const factory AppException.parseOrderFailure(String status) = ParseOrderFailure;
+}
+
+class AppExceptionData {
+  AppExceptionData(this.code, this.message);
+  final String code;
+  final String message;
+
+  @override
+  String toString() => 'AppExceptionData(code: $code, message: $message)';
+}
+
+extension AppExceptionDetails on AppException {
+  AppExceptionData get details {
+    return when(
+      // Auth
+      emailAlreadyInUse: () => AppExceptionData(
+        'email-already-in-use',
+        'Email already in use'.hardcoded,
+      ),
+      weakPassword: () => AppExceptionData(
+        'weak-password',
+        'Password is too weak'.hardcoded,
+      ),
+      wrongPassword: () => AppExceptionData(
+        'wrong-password',
+        'Wrong password'.hardcoded,
+      ),
+      userNotFound: () => AppExceptionData(
+        'user-not-found',
+        'User not found'.hardcoded,
+      ),
+      // Cart
+      cartSyncFailed: () => AppExceptionData(
+        'cart-sync-failed',
+        'An error has occurred while updating the shopping cart'.hardcoded,
+      ),
+      // Checkout
+      paymentFailureEmptyCart: () => AppExceptionData(
+        'payment-failure-empty-cart',
+        'Can\'t place an order if the cart is empty'.hardcoded,
+      ),
+      // Orders
+      parseOrderFailure: (status) => AppExceptionData(
+        'parse-order-failure',
+        'Could not parse order status: $status'.hardcoded,
+      ),
+    );
+  }
 }
