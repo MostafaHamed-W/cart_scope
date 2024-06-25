eb61eaf8fc8e5a7b2d676a1ee347969f2dedbe49
Mostafa Hamed <mostafa.hamed.w@gmail.com>
2024-06-25 20:16:47 +0300

    Update ProductsSearchState providers to use Riverpod Generator

--- lib/src/features/products/presentation/products_list/products_searach_state_provider.dart
+++ lib/src/features/products/presentation/products_list/products_searach_state_provider.dart
@@ -1,12 +1,15 @@
 import 'package:cart_scope/src/features/products/data/fake_products_repository.dart';
 import 'package:cart_scope/src/features/products/domain/product.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+part 'products_searach_state_provider.g.dart';
 
 final productsSearchQueryStateProvider = StateProvider<String>((ref) {
   return '';
 });
 
-final productsSearchResultsProvider = FutureProvider.autoDispose<List<Product>>((ref) async {
+@riverpod
+Future<List<Product>> productsSearchResults(ProductsSearchResultsRef ref) async {
   final productSearchText = ref.watch(productsSearchQueryStateProvider);
   return ref.watch(productListSerchProvider(productSearchText).future);
-});
+}
---
+++ lib/src/features/products/presentation/products_list/products_searach_state_provider.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'products_searach_state_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$productsSearchResultsHash() =>
+    r'29664b306d224be879422bb59aee147fc280479d';
+
+/// See also [productsSearchResults].
+@ProviderFor(productsSearchResults)
+final productsSearchResultsProvider =
+    AutoDisposeFutureProvider<List<Product>>.internal(
+  productsSearchResults,
+  name: r'productsSearchResultsProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$productsSearchResultsHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef ProductsSearchResultsRef = AutoDisposeFutureProviderRef<List<Product>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
