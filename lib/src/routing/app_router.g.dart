3c4f4cbb815eae5a60355e010b64b6a97eeedb3c
Mostafa Hamed <mostafa.hamed.w@gmail.com>
2024-06-25 20:17:04 +0300

    Update AppRouter providers to use Riverpod Generator

---
+++ lib/src/routing/app_router.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'app_router.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$goRouterHash() => r'1d142e48947823daac04d5dc3fd780eab29bd916';
+
+/// See also [goRouter].
+@ProviderFor(goRouter)
+final goRouterProvider = Provider<GoRouter>.internal(
+  goRouter,
+  name: r'goRouterProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$goRouterHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef GoRouterRef = ProviderRef<GoRouter>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
