11cf684a6b38b89c686d1ac3ebbb85bc87193407
Mostafa Hamed <mostafa.hamed.w@gmail.com>
2024-06-25 18:25:59 +0300

    Update FakeAuthRepository providers to use Riverpod Generator

--- lib/src/features/authentication/data/fake_auth_repository.dart
+++ lib/src/features/authentication/data/fake_auth_repository.dart
@@ -3,7 +3,8 @@ import 'package:cart_scope/src/features/authentication/domain/app_user.dart';
 import 'package:cart_scope/src/features/authentication/domain/fake_app_user.dart';
 import 'package:cart_scope/src/utils/delay.dart';
 import 'package:cart_scope/src/utils/in_memory_store.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+part 'fake_auth_repository.g.dart';
 
 class FakeAuthRepository {
   FakeAuthRepository({this.addDelay = true});
@@ -69,13 +70,15 @@ class FakeAuthRepository {
   }
 }
 
-final authRepositoryProvider = Provider<FakeAuthRepository>((ref) {
+@Riverpod(keepAlive: true)
+FakeAuthRepository authRepository(AuthRepositoryRef ref) {
   final auth = FakeAuthRepository();
   ref.onDispose(() => auth.dispose());
   return auth;
-});
+}
 
-final authStateChangesProvider = StreamProvider<AppUser?>((ref) {
+@Riverpod(keepAlive: true)
+Stream<AppUser?> authStateChanges(AuthStateChangesRef ref) {
   final authRepository = ref.watch(authRepositoryProvider);
   return authRepository.authStateChanges();
-});
+}
