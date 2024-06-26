d6778c60089da83bb9dc69f679081bdbb2683ffa
Mostafa Hamed <mostafa.hamed.w@gmail.com>
2024-06-26 23:09:52 +0300

    Migrating LeaveReviewController to AsyncNotifier using Riverpod Generator [skip ci]

--- lib/src/features/reviews/presentation/leave_review_screen/leave_review_controller.dart
+++ lib/src/features/reviews/presentation/leave_review_screen/leave_review_controller.dart
@@ -2,25 +2,30 @@ import 'package:cart_scope/src/features/products/domain/product.dart';
 import 'package:cart_scope/src/features/reviews/application/reviews_service.dart';
 import 'package:cart_scope/src/features/reviews/domain/review.dart';
 import 'package:cart_scope/src/utils/current_date_provider.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-class LeaveReviewController extends StateNotifier<AsyncValue<void>> {
-  LeaveReviewController({
-    required this.reviewsService,
-    required this.currentDateBuilder,
-  }) : super(const AsyncData(null));
-  final ReviewsService reviewsService;
-  // * this is injected so we can easily mock the date in the tests
-  final DateTime Function() currentDateBuilder;
+part 'leave_review_controller.g.dart';
+
+@riverpod
+class LeaveReviewController extends _$LeaveReviewController {
+  bool mounted = true;
+  @override
+  FutureOr<void> build() {
+    ref.onDispose(() => mounted = false);
+    // nothing to do
+  }
 
   Future<void> submitReview({
+    Review? previousReview,
     required ProductID productId,
     required double rating,
     required String comment,
     required void Function() onSuccess,
-    required Review? previousReview,
   }) async {
-    if (previousReview == null || comment != previousReview.comment || rating != previousReview.rating) {
+    // * only submit if the rating is new or it has changed
+    if (previousReview == null || rating != previousReview.rating || comment != previousReview.comment) {
+      final currentDateBuilder = ref.read(currentDateBuilderProvider);
+      final reviewsService = ref.read(reviewsServiceProvider);
       final review = Review(
         rating: rating,
         comment: comment,
@@ -29,7 +34,7 @@ class LeaveReviewController extends StateNotifier<AsyncValue<void>> {
       state = const AsyncLoading();
       final newState = await AsyncValue.guard(() => reviewsService.submitReview(productId: productId, review: review));
       if (mounted) {
-        // only set stae if the controller has not been disposed
+        // * only set the state if the controller hasn't been disposed
         state = newState;
         if (state.hasError == false) {
           onSuccess();
@@ -40,10 +45,3 @@ class LeaveReviewController extends StateNotifier<AsyncValue<void>> {
     }
   }
 }
-
-final leaveReviewControllerProvider = StateNotifierProvider.autoDispose<LeaveReviewController, AsyncValue<void>>((ref) {
-  return LeaveReviewController(
-    reviewsService: ref.watch(reviewsServiceProvider),
-    currentDateBuilder: ref.watch(currentDateBuilderProvider),
-  );
-});
