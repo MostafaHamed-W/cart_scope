ae0a2a5927c6f1c060e709124413a187f1299d12
Mostafa Hamed <mostafa.hamed.w@gmail.com>
2024-06-26 23:13:50 +0300

    Migrating ShoppingCartScreenController to AsyncNotifier using Riverpod Generator [skip ci]

--- lib/src/features/cart/presentation/shopping_cart/shopping_cart_screen_controller.dart
+++ lib/src/features/cart/presentation/shopping_cart/shopping_cart_screen_controller.dart
@@ -1,27 +1,28 @@
+import 'dart:async';
 import 'package:cart_scope/src/features/cart/application/cart_service.dart';
 import 'package:cart_scope/src/features/cart/domain/item.dart';
 import 'package:cart_scope/src/features/products/domain/product.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-class ShoppingCartScreenController extends StateNotifier<AsyncValue<void>> {
-  ShoppingCartScreenController({required this.cartService}) : super(const AsyncData(null));
-  final CartService cartService;
+part 'shopping_cart_screen_controller.g.dart';
+
+@riverpod
+class ShoppingCartScreenController extends _$ShoppingCartScreenController {
+  @override
+  FutureOr<void> build() {
+    //no thing to do
+  }
 
   Future<void> updateItemQuantity(ProductID productId, int quantity) async {
+    final cartService = ref.watch(cartServiceProvider);
     state = const AsyncLoading();
     final updated = Item(productId: productId, quantity: quantity);
     state = await AsyncValue.guard(() => cartService.setItem(updated));
   }
 
   Future<void> removeItemById(ProductID productId) async {
+    final cartService = ref.watch(cartServiceProvider);
     state = const AsyncLoading();
     state = await AsyncValue.guard(() => cartService.removeItemById(productId));
   }
 }
-
-final shoppingCartScreenControllerProvider =
-    StateNotifierProvider<ShoppingCartScreenController, AsyncValue<void>>((ref) {
-  return ShoppingCartScreenController(
-    cartService: ref.watch(cartServiceProvider),
-  );
-});
---
+++ lib/src/features/cart/presentation/shopping_cart/shopping_cart_screen_controller.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'shopping_cart_screen_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$shoppingCartScreenControllerHash() =>
+    r'b2a36bd688bdcec2e92d98450f5edac5f7aced6c';
+
+/// See also [ShoppingCartScreenController].
+@ProviderFor(ShoppingCartScreenController)
+final shoppingCartScreenControllerProvider = AutoDisposeAsyncNotifierProvider<
+    ShoppingCartScreenController, void>.internal(
+  ShoppingCartScreenController.new,
+  name: r'shoppingCartScreenControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$shoppingCartScreenControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$ShoppingCartScreenController = AutoDisposeAsyncNotifier<void>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
